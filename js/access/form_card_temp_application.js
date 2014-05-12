4725464e677b4618144fc8a5d4225ec8c1e6c3f1
eewindfly <eewindfly@gmail.com>
2014-05-12 20:11:24 +0800

    test

--- js/access/form_card_temp_application.js
+++ js/access/form_card_temp_application.js
@@ -13,10 +13,10 @@
 			}
 		});
 		$("select[name='application_type']").change(function(){
-			var type_no = $(this).find("option:selected").val();
-			$("select[name='guest_purpose']").remove("option");
-			for(var i=0;i<jdata[type_no].purpose.length;i++){
-				$("select[name='guest_purpose']").append('<option value'+jdata[type_no].purpose[i].purpose_no+'>'+jdata[type_no].purpose[i].purpose_name+'</option>');
+			var idx = $(this).find("option:selected").index();
+			$("select[name='guest_purpose']").empty();
+			for(var i=0;i<jdata[idx].purpose.length;i++){
+				$("select[name='guest_purpose']").append('<option value'+jdata[idx].purpose[i].purpose_no+'>'+jdata[idx].purpose[i].purpose_name+'</option>');
 			}
 		});
 	});
