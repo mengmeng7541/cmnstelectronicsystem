f649572444867652cd90d11a94c375a0a4961d29
eewindfly <eewindfly@gmail.com>
2014-05-12 18:30:19 +0800

    fix

--- application/views/access/form_card_temp_application.php
+++ application/views/access/form_card_temp_application.php
@@ -104,14 +104,14 @@
 			dataType: 'JSON',
 		}).always(function(data){
 			for(var i=0;i<data.length;i++){
-				$("select[name='application_type']").append('<option value'+data[i].type_no+'>'+data[i].type_name+'</option>'));
+				$("select[name='application_type']").append('<option value'+data[i].type_no+'>'+data[i].type_name+'</option>');
 			}
 		});
 		$("select[name='application_type']").change(function(){
 			var type_no = $(this).find("option:selected").val();
 			$("select[name='guest_purpose']").remove("option");
 			for(var i=0;i<data[type_no].purpose.length;i++){
-				$("select[name='guest_purpose']").append('<option value'+data[type_no].purpose[i].purpose_no+'>'+data[type_no].purpose[i].purpose_name+'</option>'));
+				$("select[name='guest_purpose']").append('<option value'+data[type_no].purpose[i].purpose_no+'>'+data[type_no].purpose[i].purpose_name+'</option>');
 			}
 		});
 	});
