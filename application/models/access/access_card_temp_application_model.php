ef23d15d30afb0ea40dd19a946df15813329a7a5
eewindfly <eewindfly@gmail.com>
2014-05-13 21:44:01 +0800

    fix

--- application/models/access/access_card_temp_application_model.php
+++ application/models/access/access_card_temp_application_model.php
@@ -86,7 +86,8 @@ class Access_card_temp_application_model extends MY_Model {
 		if($data['application_type_ID']=="guest"){
 			//寫入資料
 			foreach((array)$data['guest_name'] as $idx => $guest_name){
-				if(empty($guest_name = trim($guest_name))) continue;//如果是空的就略過
+				$guest_name = trim($guest_name);
+				if(empty($guest_name)) continue;//如果是空的就略過
 				$insert_id = $this->access_model->add_access_card_temp_application(array(
 					"applied_by"=>$this->session->userdata('ID'),
 					"application_type"=>$type['type_no'],
