610770880a10c2e5aa9addf034bf11600f6802c4
eewindfly <eewindfly@gmail.com>
2014-05-17 13:29:54 +0800

    test

--- application/views/user/list_clock.php
+++ application/views/user/list_clock.php
@@ -112,7 +112,7 @@
 				);
 			}
 			$("#table_user_clock_list td").css("width",100/columns_per_row+"%");
-			$("#table_user_clock_list td").fitText();
+			$("#table_user_clock_list td").fitText(4);
 		});
 	};
 	$(document).ready(function(){
