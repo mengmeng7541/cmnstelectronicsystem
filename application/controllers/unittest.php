cd554e03a144abd4e8d8fbb9e07d6be162145991
eewindfly <eewindfly@gmail.com>
2014-05-22 16:54:41 +0800

    for test

--- application/controllers/unittest.php
+++ application/controllers/unittest.php
@@ -15,50 +15,51 @@ class Unittest extends MY_Controller {
 		$this->load->model('curriculum_model');
 //		$this->benchmark->mark('code_start');
 		$this->load->model('curriculum/reg_model');
-		$this->reg_model->refresh_rank();
-//				$options = array(
-//					"class_code"=>'2014-01'
-//				);
-//				$classes = $this->curriculum_model->get_class_list($options)->result_array();
-//				
-//				$this->load->model('facility_model');
-//				foreach($classes as $class)
-//				{
-//					
-//					$row = array();
-//					$row[] = "{$class['course_cht_name']} ({$class['course_eng_name']})";
-//					$row[] = end(explode("-",$class['class_code']));
-//					$row[] = $this->curriculum_model->get_class_type_str($class['class_type']);
-//					$row[] = $class['location_cht_name'];
-//					$row[] = $class['class_start_time'];
-//					$row[] = $class['class_end_time'];
-//					$row[] = $class['class_total_secs']/3600;
-//					$row[] = $class['prof_name'];
-//					$reg_participants = $this->curriculum_model->get_reg_list(array("class_ID"=>$class['class_ID']))->num_rows();
-//					$row[] = "$reg_participants/{$class['class_max_participants']}";
-//					$row[] = self::$class_state[$class['class_state']];
-//					$row[] = $class['class_remark'];
-//					
-//					if(!$this->curriculum_model->is_super_admin())
-//					{
-//						$facility_IDs = $this->course_model->get_course_map_facility_ID($class['course_ID']);
-//						if(!empty($facility_IDs)){
-//							$privilege = $this->facility_model->get_user_privilege_list(array("facility_ID"=>$facility_IDs,"privilege"=>"admin","user_ID"=>$this->session->userdata('ID')))->result_array();
-//							if(!$privilege){
-//								continue;
-//							}
-//						}
-//						
-//					}
-//					$row[] = implode(' ',array(
-//						anchor("/curriculum/class/edit/".$class['class_ID'],"編輯","class='btn btn-warning btn-small'"),
-//						form_button("del_class","刪除","class='btn btn-danger btn-small' value='{$class['class_ID']}'"),
-//						anchor("/curriculum/reg/form/".$class['class_ID'],"加選","class='btn btn-primary btn-small'"),
-//						anchor("/curriculum/lesson/list/".$class['class_ID'],"排課","class='btn btn-primary btn-small'")
-//					));
-//					
-//					$output['aaData'][] = $row;
-//				}
+		
+				$options = array(
+					"class_code"=>'2014-05'
+				);
+				$classes = $this->curriculum_model->get_class_list($options)->result_array();
+				
+				$this->load->model('facility_model');
+				foreach($classes as $class)
+				{
+					
+					$row = array();
+					$row[] = "{$class['course_cht_name']} ({$class['course_eng_name']})";
+					$row[] = end(explode("-",$class['class_code']));
+					$row[] = $this->curriculum_model->get_class_type_str($class['class_type']);
+					$row[] = $class['location_cht_name'];
+					$row[] = $class['class_start_time'];
+					$row[] = $class['class_end_time'];
+					$row[] = $class['class_total_secs']/3600;
+					$row[] = $class['prof_name'];
+					$reg_participants = $this->curriculum_model->get_reg_list(array("class_ID"=>$class['class_ID']))->num_rows();
+					$row[] = "$reg_participants/{$class['class_max_participants']}";
+					$row[] = self::$class_state[$class['class_state']];
+					$row[] = $class['class_remark'];
+					
+					if(!$this->curriculum_model->is_super_admin())
+					{
+						$facility_IDs = $this->course_model->get_course_map_facility_ID($class['course_ID']);
+						if(!empty($facility_IDs)){
+							$privilege = $this->facility_model->get_user_privilege_list(array("facility_ID"=>$facility_IDs,"privilege"=>"admin","user_ID"=>$this->session->userdata('ID')))->result_array();
+							if(!$privilege){
+								continue;
+							}
+						}
+						
+					}
+					$row[] = implode(' ',array(
+						anchor("/curriculum/class/edit/".$class['class_ID'],"編輯","class='btn btn-warning btn-small'"),
+						form_button("del_class","刪除","class='btn btn-danger btn-small' value='{$class['class_ID']}'"),
+						anchor("/curriculum/reg/form/".$class['class_ID'],"加選","class='btn btn-primary btn-small'"),
+						anchor("/curriculum/lesson/list/".$class['class_ID'],"排課","class='btn btn-primary btn-small'")
+					));
+					
+					$output['aaData'][] = $row;
+					break;
+				}
 		$this->benchmark->mark('code_end');
 		
 		
@@ -120,6 +121,9 @@ class Unittest extends MY_Controller {
 			}
 //			return;
 		}
+		
+		$this->load->model('curriculum/reg_model');
+		$this->reg_model->refresh_rank();
 	}
   
 //  public function form()
