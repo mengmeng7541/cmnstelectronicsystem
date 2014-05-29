f94004c164d12f05f27b6b396eedd71db029f1a6
eewindfly <eewindfly@gmail.com>
2014-05-29 13:43:27 +0800

    fix

--- application/controllers/unittest.php
+++ application/controllers/unittest.php
@@ -30,8 +30,8 @@ class Unittest extends MY_Controller {
 		//先刪除舊的
 		$curriculum_db->where("class_code <=","2014-04-E");
 		$curriculum_db->delete("class_list");
-		$curriculum_db->where("lesson_start_time <=","2014-04-30 00:00:00");
-		$curriculum_db->delete("lesson_list");
+//		$curriculum_db->where("lesson_start_time <=","2014-04-30 00:00:00");
+//		$curriculum_db->delete("lesson_list");
 //		return;
 		
 		$curriculum_db->select("SN,user_ID,class_code,class_name,course_ID,MIN(reg_time) AS reg_time,MAX(join_check) AS join_check,su,MAX(cashornot) AS cashornot,class_date");
