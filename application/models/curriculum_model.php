8377e84f5659d70f04ced74027c04c3226ca81d0
eewindfly <eewindfly@gmail.com>
2014-06-11 10:51:14 +0800

    change class reg_state to 'canceled' when class reg is deleted.

--- application/models/curriculum_model.php
+++ application/models/curriculum_model.php
@@ -548,6 +548,8 @@ class Curriculum_model extends MY_Model {
 			$this->curriculum_db->where("{$sJoinTable['class']}.class_code",$data['class_code']);
 		if(isset($data['reg_state']))
 			$this->curriculum_db->where("$sTable.reg_state",$data['reg_state']);
+		else
+			$this->curriculum_db->where("$sTable.reg_state",'canceled');
 		if(isset($data['reg_ID']))
 			$this->curriculum_db->where("$sTable.reg_ID",$data['reg_ID']);
 		$this->curriculum_db->set("$sTable.reg_canceled_by",isset($data['reg_canceled_by'])?$data['reg_canceled_by']:$this->session->userdata('ID'));
